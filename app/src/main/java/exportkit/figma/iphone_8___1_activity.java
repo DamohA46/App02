845e02acb7d1bf3245179f031847d78c7164d270
Ahmed Hekal <=>
2022-07-25 19:08:14 +0100

    first

---
+++ app/src/main/java/exportkit/figma/iphone_8___1_activity.java
@@ -0,0 +1,48 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		iphone_8___1
+	 *	@date 		Friday 22nd of July 2022 11:22:43 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+
+public class iphone_8___1_activity extends Activity {
+
+	
+	private View _bg__iphone_8___1_ek2;
+	private View rectangle_8;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.iphone_8___1);
+
+		
+		_bg__iphone_8___1_ek2 = (View) findViewById(R.id._bg__iphone_8___1_ek2);
+		rectangle_8 = (View) findViewById(R.id.rectangle_8);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
